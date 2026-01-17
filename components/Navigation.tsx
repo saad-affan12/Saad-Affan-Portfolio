da99c2085a839de0726065af387c50cb433e833e
Mohammed Saad Affan A <saadaffan129@gmail.com>
2026-01-17 20:08:33 +0530

    Fix _app.tsx

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,69 +1,84 @@
-<Nav className="align-items-lg-center ml-lg-auto" navbar>
-  {/* Section Navigation */}
-  <NavItem>
-    <NavLink href="#home">Home</NavLink>
-  </NavItem>
+import React from "react";
+import {
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
 
-  <NavItem>
-    <NavLink href="#skills">Skills</NavLink>
-  </NavItem>
+import { socialLinks } from "../portfolio";
 
-  <NavItem>
-    <NavLink href="#education">Education</NavLink>
-  </NavItem>
+const Navigation: React.FC = () => {
+  return (
+    <Nav className="align-items-lg-center ml-lg-auto" navbar>
+      {/* Section Navigation */}
+      <NavItem>
+        <NavLink href="#home">Home</NavLink>
+      </NavItem>
 
-  <NavItem>
-    <NavLink href="#experience">Experience</NavLink>
-  </NavItem>
+      <NavItem>
+        <NavLink href="#skills">Skills</NavLink>
+      </NavItem>
 
-  <NavItem>
-    <NavLink href="#projects">Projects</NavLink>
-  </NavItem>
+      <NavItem>
+        <NavLink href="#education">Education</NavLink>
+      </NavItem>
 
-  <NavItem>
-    <NavLink href="#contact">Contact</NavLink>
-  </NavItem>
+      <NavItem>
+        <NavLink href="#experience">Experience</NavLink>
+      </NavItem>
 
-  {/* Social Icons */}
-  {socialLinks.github && (
-    <NavItem>
-      <NavLink
-        rel="noopener"
-        aria-label="GitHub"
-        className="nav-link-icon"
-        href={socialLinks.github}
-        target="_blank"
-      >
-        <i className="fa fa-github" />
-      </NavLink>
-    </NavItem>
-  )}
+      <NavItem>
+        <NavLink href="#projects">Projects</NavLink>
+      </NavItem>
 
-  {socialLinks.linkedin && (
-    <NavItem>
-      <NavLink
-        rel="noopener"
-        aria-label="LinkedIn"
-        className="nav-link-icon"
-        href={socialLinks.linkedin}
-        target="_blank"
-      >
-        <i className="fa fa-linkedin" />
-      </NavLink>
-    </NavItem>
-  )}
+      <NavItem>
+        <NavLink href="#contact">Contact</NavLink>
+      </NavItem>
 
-  {socialLinks.instagram && (
-    <NavItem>
-      <NavLink
-        rel="noopener"
-        aria-label="Instagram"
-        className="nav-link-icon"
-        href={socialLinks.instagram}
-        target="_blank"
-      >
-        <i className="fa fa-instagram" />
-      </NavLink>
-    </NavItem>
-  )}
-</Nav>
+      {/* Social Icons */}
+      {socialLinks.github && (
+        <NavItem>
+          <NavLink
+            rel="noopener"
+            aria-label="GitHub"
+            className="nav-link-icon"
+            href={socialLinks.github}
+            target="_blank"
+          >
+            <i className="fa fa-github" />
+          </NavLink>
+        </NavItem>
+      )}
+
+      {socialLinks.linkedin && (
+        <NavItem>
+          <NavLink
+            rel="noopener"
+            aria-label="LinkedIn"
+            className="nav-link-icon"
+            href={socialLinks.linkedin}
+            target="_blank"
+          >
+            <i className="fa fa-linkedin" />
+          </NavLink>
+        </NavItem>
+      )}
+
+      {socialLinks.instagram && (
+        <NavItem>
+          <NavLink
+            rel="noopener"
+            aria-label="Instagram"
+            className="nav-link-icon"
+            href={socialLinks.instagram}
+            target="_blank"
+          >
+            <i className="fa fa-instagram" />
+          </NavLink>
+        </NavItem>
+      )}
+    </Nav>
+  );
+};
+
+export default Navigation;
